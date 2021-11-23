ce892987d7d8f51cc6406ea86907985943699b98
Rafael De Lima <rafael-silva420@hotmail.com>
2021-11-23 09:19:32 -0300

    commit 2

--- index.js
+++ index.js
@@ -1,9 +1,13 @@
+// importar Express
 const express = require("express");
+// Importando o roteador
+
 const PizzasRouter = require("./routers/PizzasRouter");
+// Criar o servidor
 const app = express();
-
+// Criar a rota respondendo a requisição
 app.use('/', PizzasRouter);
-
+// Levantando o servidor
 app.listen(3000, ()=>{
     console.log("servidor rodando...")
 });
